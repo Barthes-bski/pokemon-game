aa1560fcc2ea82d4d55e387186bed232b2944aa4
Matias Soto <matias.delfos.l@gmail.com>
2023-04-13 21:59:37 -0400

    Inicio clase 93

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1> ¿Quién es este Pokémon?</h1>
+
+    <PokemonPicture :pokemonId="4" :showPokemon="true"/>
+    <PokemonOptions />
+
+</template>
+
+
+
+<script>
+    import PokemonOptions from '@/components/PokemonOptions'
+    import PokemonPicture from '@/components/PokemonPicture'
+
+    import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+    export default {
+        components: { 
+            PokemonOptions,
+            PokemonPicture
+        }
+    }
+</script>
+
+<style>
+</style>
